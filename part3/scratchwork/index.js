1173fc663109215589f34fe9513e1d95857adb0a
Brenden Cheung <cheungbrenden@gmail.com>
2023-11-25 16:45:13 -0800

    more small changes

--- part3/scratchwork/index.js
+++ part3/scratchwork/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+
 const app = express()
 
 
@@ -13,6 +14,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 // app.use(requestLogger)
 
 let notes = [
