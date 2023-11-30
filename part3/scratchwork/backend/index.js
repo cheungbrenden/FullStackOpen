129f8782a28bbf9c277b09cea33d369a998feceb
Brenden Cheung <cheungbrenden@gmail.com>
2023-11-30 11:25:22 -0800

    uibuild

--- part3/scratchwork/backend/index.js
+++ part3/scratchwork/backend/index.js
@@ -1,11 +1,9 @@
-require('dotenv').config()
 const express = require('express')
-const cors = require('cors')
-
 const app = express()
+const cors = require('cors')
+require('dotenv').config()
 
 const Note = require('./models/note')
-
 
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
@@ -15,32 +13,10 @@ const requestLogger = (request, response, next) => {
     next()
 }
 
-app.use(cors())
 app.use(express.static('dist'))
 app.use(express.json())
 app.use(requestLogger)
-
-let notes = [
-    {
-        id: 1,
-        content: "HTML is easy",
-        important: true
-    },
-    {
-        id: 2,
-        content: "Browser can execute only JavaScript",
-        important: false
-    },
-    {
-        id: 3,
-        content: "GET and POST are the most important methods of HTTP protocol",
-        important: true
-    }
-]
-
-// app.get('/', (request, response) => {
-//     response.send('<h1>Hello World!</h1>')
-// })
+app.use(cors())
 
 app.post('/api/notes', (request, response) => {
     const body = request.body
